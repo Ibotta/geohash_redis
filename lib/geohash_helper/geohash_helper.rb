d0a21230974221a68e8aeaf134f4a9cf79571eb2
Mengxiang Jiang <mj294@cornell.edu>
2015-09-25 10:30:30 -0600

    comments

--- lib/geohash_helper/geohash_helper.rb
+++ lib/geohash_helper/geohash_helper.rb
@@ -1,7 +1,17 @@
 require 'ffi'
 require 'ffi-compiler/loader'
 
+# ruby-ffi requires that arguments and the return values of c functions
+# must either be primitive types or pointers.
+# Therefore many of the structs that are passed as arguments and returned
+# were changed to pointers for those functions
+# Some of the structs themselves were also changed for easier access by ffi
+
 module GeohashHelper
+  
+  # GeoHashRadius's members were changed from structs to pointers
+  # If I were to keep the members as structs, I would need to write getter methods
+  # and the methods would return pointers to the structs anyways
   class GeoHashRadius < FFI::ManagedStruct
     layout :hash,  :pointer,
            :area,  :pointer,
@@ -12,6 +22,7 @@ module GeohashHelper
     end
   end
 
+  # GeoHashBits remained the same as before since its members are already primitive types
   class GeoHashBits < FFI::ManagedStruct
     layout :bits,  :__uint64_t,
            :step,  :__uint8_t
@@ -20,7 +31,7 @@ module GeohashHelper
       GeohashHelper.free_object(ptr)
     end
   end
-
+  # GeoHashRange remained the same as before since its members are already primitive types
   class GeoHashRange < FFI::ManagedStruct
     layout :min,  :double,
            :max,  :double
@@ -30,6 +41,7 @@ module GeohashHelper
     end
   end
 
+  # GeoHashArea's members were changed from structs to pointers for the same reason as GeoHashRadius
   class GeoHashArea < FFI::ManagedStruct
     layout :hash,  :pointer,
            :longitude,  :pointer,
@@ -40,6 +52,7 @@ module GeohashHelper
     end
   end
 
+  # GeoHashNeighbors's members were changed from structs to pointers for the same reason as GeoHashRadius
   class GeoHashNeighbors < FFI::ManagedStruct
     layout :north,  :pointer,
            :east,  :pointer,
@@ -57,13 +70,39 @@ module GeohashHelper
 
   extend FFI::Library
   ffi_lib FFI::Compiler::Loader.find('geohash_helper')
+
+  # converts geo coords and a radius into an area
+  # inputs are longitude, latitude, and radius (in meters)
+  # output is a pointer to a GeoHashArea
   attach_function :geohashGetAreasByRadius, [:double, :double, :double], :pointer
+  
+  # converts geo coords into a geohash string
+  # inputs are longitude, latitude, and string length
+  # output is a geohash string
   attach_function :geohashEncodeToStr, [:double, :double, :int], :string
+  
+  # converts a geohash string to an area
+  # input is a geohash string
+  # output is a pointer to a GeoHashArea
   attach_function :geohashDecodeToArea, [:string], :pointer
+  
+  # converts a geohash bits value to a geohash string
+  # input is a pointer to a GeoHashBits
+  # output is a geohash string
   attach_function :geohashBitsToStr, [:pointer], :string
+  
+  # converts a geohash stringto a geohash bits value
+  # input is a geohash string
+  # output is a pointer to a GeoHashBits
   attach_function :geohashStrToBits, [:string], :pointer
+  
+  # gets the neighbors of a geohash given its string
+  # input is a geohash string
+  # output is a pointer to a GeoHashNeighbors
   attach_function :geohashNeighborsFromStr, [:string], :pointer
 
+  # functions from the original c files
+  # slight modifications to take in and return pointers instead of structs
   attach_function :geohashEncode, [:pointer, :pointer, :double, :double, :__uint8_t, :pointer], :int
   attach_function :geohashEncodeType, [:double, :double, :__uint8_t, :pointer], :int
   attach_function :geohashEncodeWGS84, [:double, :double, :__uint8_t, :pointer], :int
